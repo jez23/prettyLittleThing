00b80e1c0e408c31e02da462ff7ac035c45d0a61
Jeremy Blackmore <jeremyblackmore@iMac.local>
2021-01-09 23:13:01 +0000

    text amend

--- src/components/SubTotal.test.js
+++ src/components/SubTotal.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react'
-import SubTotal from './subTotal';
+import SubTotal from './SubTotal';
 
 describe('Render component', () => {
     test("should render the SubTotal component", () => {
